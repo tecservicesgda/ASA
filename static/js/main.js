bfccd35e766156599c23400762f7fa3a8dc94725
tecservicesgda <112429879+tecservicesgda@users.noreply.github.com>
2025-06-09 17:33:35 -0600

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('submitBtn').addEventListener('click', function () {
+    // This will trigger the API call once implemented
+    alert('Button clicked! The API integration will happen here.');
+});
